5199e03f60a439481ca24698fb8adad3976ccac0
Enoc-Mena <77991403+Enoc-Mena@users.noreply.github.com>
2021-06-02 14:23:08 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.paintcalculator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double length, width;
+
+        System.out.print("One gallon of paint covers 350 square feet.");
+        System.out.println("");
+
+        //Gets length
+        Scanner lengthOne = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        length = lengthOne.nextDouble();
+
+        //Gets width
+        Scanner widthOne = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        width = widthOne.nextDouble();
+
+        double total = length * width;
+
+        //Divide into 350 to get decimal that will be converted to nearest whole number
+        double gallonsNeeded = total / 350; 
+
+        System.out.print("You will need " + Math.ceil(gallonsNeeded) + " gallons to cover " + total + " feet.");
+
+    }
+}
